780d10a98da0b6dc23de1642f1d7e0cf3bd75f62
Wes Bos <wesbos@gmail.com>
2017-11-15 13:07:50 -0500

    ba

--- gulpfile.js
+++
@@ -1,9 +0,0 @@
-const gulp = require('gulp');
-const ico = require('gulp-to-ico');
-
-gulp.task('ico', () =>
-  gulp
-    .src('./images/*.png')
-    .pipe(ico())
-    .pipe(gulp.dest('./dist/'))
-);
